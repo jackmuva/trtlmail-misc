0ef856374cf7fa860dd238364413c0db928a2aa9
jackmuva <33766184+jackmuva@users.noreply.github.com>
2024-03-14 16:30:02 -0500

    Created a lambda function for triggering service to spin up and spin down

---
+++ email-task-lambda/email-task-lambda.js
@@ -0,0 +1,27 @@
+const aws = require('aws-sdk');
+exports.handler = (event, context) => {
+    // stop all taks
+    var ecsRegion = 'us-east-1';
+    if(event.status == 'stop'){
+        var params = {
+            cluster: process.env.ECS_CLUSTER,
+            service: process.env.ECS_SERVICE_NAME,
+            desiredCount: 0
+        };
+    }
+    else{
+        // otherwise : start one task
+        var params = {
+            cluster: process.env.ECS_CLUSTER,
+            service: process.env.ECS_SERVICE_NAME,
+            desiredCount: 1
+        };
+    }
+    var ecs = new aws.ECS({region: ecsRegion});
+    ecs.updateService(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            context.succeed();
+        }
+    });
+};
